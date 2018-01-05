c77239e25c906fcb62d95eafd6813e0914ab469f
Jake Snell <jsnell@n.io>
2018-01-05 09:28:58 -0700

    Update block to handle dynamic input

--- google_maps_geocode.py
+++ google_maps_geocode.py
@@ -9,17 +9,16 @@ class GoogleMapsGeocode(Block):
 
     version = VersionProperty('0.1.0')
     api_key = Property(title='API Key', default='[[GOOGLE_MAPS_API_KEY]]')
-    address = StringProperty(title="Street Address", default="")
+    address = Property(title="Street Address", default="")
 
     def process_signals(self, signals):
         lng=0.0
         lat=0.0
 
         for signal in signals:
-            print(self.api_key())
-            print(self.address())
+
             gmaps = googlemaps.Client(key=self.api_key())
-            geocode_result = gmaps.geocode(str(self.address()))
+            geocode_result = gmaps.geocode(str(self.address(signal)))
             lng = geocode_result[0].get('geometry')['location'].get('lng')
             lat = geocode_result[0].get('geometry')['location'].get('lat')
 
